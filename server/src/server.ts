35b65c76b38517e8e721e71fcfcaec46394a60b4
osci-the-orca <88oscste@skola.boras.se>
2022-08-20 13:37:54 +0200

    created router, controller and added router to server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import { charactersRouter } from "./resources/characters/characters.router";
 require("express-async-errors");
 
 
 const app = express();
+
+app.use(express.json())
+
+
+app.use("/",charactersRouter);
 
 app.listen(3000, () => console.log("running on http://localhost:3000"));
