99e0a16c8ad5c67b020ff12cae5efbb3af57e6f5
osci-the-orca <88oscste@skola.boras.se>
2022-08-26 21:04:11 +0200

    formatting

--- server/src/resources/characters/characters.controller.ts
+++ server/src/resources/characters/characters.controller.ts
@@ -18,8 +18,7 @@ export const getCharacterById = (req: Request, res: Response, next: NextFunction
 
 export const createCharacter = async (req: Request, res: Response) => {
 
-    if(!req.body.image || !req.body.image.includes(".jpg") && !req.body.image.includes(".png"))
-    {
+    if(!req.body.image || !req.body.image.includes(".jpg") && !req.body.image.includes(".png")) {
         req.body.image = "https://rickandmortyapi.com/api/character/avatar/19.jpeg";
     }
     
@@ -40,20 +39,19 @@ export const deleteCharacter = async (req: Request, res: Response, next: NextFun
 
         await saveData('./src/resources/characters/rickAndMortyData.json');
 
-        res.status(200).json(character);
+        res.status(204).json();
     }
     else next();
 };
 
 export const updateCharacter = async (req: Request, res: Response, next: NextFunction) => {
     
-    const id = req.params.id
-    const character = characters.find(x => x.id === id);
+    const character = characters.find(x => x.id === req.params.id);
 
     if(character){
         const index = characters.indexOf(character);
 
-        characters[index] = {id,...req.body };
+        characters[index] = req.body;
 
        await saveData("./src/resources/characters/rickAndMortyData.json")
         res.status(200).json(characters[index]);
@@ -67,7 +65,6 @@ export const validateCharacterReqBody = (req: Request, res: Response, next: Next
     if(result.error) {
         res.status(400).json(result.error.message);
     }
-    else {
-        next();
-    }
+    else next();
+        
 };
